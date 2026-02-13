b289ebff30037493a4b7c3e6abab5e5d25938880
Leonardo <nardoso@gmail.com>
2026-02-13 03:09:41 -0300

    First commit: \nStarting

---
+++ app/models/travel.py
@@ -0,0 +1,68 @@
+from datetime import datetime
+from decimal import Decimal
+from sqlalchemy import String, Numeric, Float, DateTime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Travel(Base):
+    __tablename__ = "travels"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+    total_value: Mapped[Decimal] = mapped_column(
+        Numeric(10, 2), 
+        nullable=False
+    )
+
+    street_start: Mapped[str] = mapped_column(
+        String(255), 
+        nullable=False
+    )
+
+    neiborghood_start: Mapped[str] = mapped_column(
+        String(255), 
+        nullable=False
+    )
+
+    street_end: Mapped[str] = mapped_column(
+        String(255), 
+        nullable=False
+    )
+
+    neiborghood_end: Mapped[str] = mapped_column(
+        String(255), 
+        nullable=False
+    )
+
+    distance_start: Mapped[float] = mapped_column(
+        Float, 
+        nullable=False
+    )
+
+    distance_travel: Mapped[float] = mapped_column(
+        Float, 
+        nullable=False
+    )
+
+    time_start: Mapped[datetime] = mapped_column(
+        DateTime, 
+        nullable=False
+    )
+
+    time_reach: Mapped[datetime] = mapped_column(
+        DateTime, 
+        nullable=False
+    )
+
+    time_end: Mapped[datetime] = mapped_column(
+        DateTime, 
+        nullable=False
+    )
+
+    time_total: Mapped[int] = mapped_column(
+        nullable=False
+    )
